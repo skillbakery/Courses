c6b87785ca795d76210324f16851a7d926d32dfe
Kunwar Singh <kunwar@datagainservices.com>
2024-07-17 18:46:28 +0530

    ASP.Net Core Session Sample
    ASP.Net Core Session Sample

---
+++ ASP,NetCore-SessionSample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SessionSample.Models;
+using System.Diagnostics;
+
+namespace SessionSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        //Using Session
+        private readonly ISession _session;
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _session = httpContextAccessor.HttpContext.Session;
+        }
+
+        public IActionResult Index()
+        {
+            // Store data in session
+            _session.SetString("UserName", "JohnDoe");
+            // Retrieve data from session
+            var userName = _session.GetString("UserName");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
